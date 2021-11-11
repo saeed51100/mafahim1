964729d1ea871942f5a7471b2c6d72a61655db17
saeed <saeedblack@gmail.com>
2021-11-11 10:27:52 -0500

    If you do not want to register, you must change routes.

--- routes/web.php
+++ routes/web.php
@@ -82,8 +82,10 @@ Route::post(
 // disable register:
 // Auth::routes(['register' => false]);
 // https://redcherry.ir/%d8%a2%d9%85%d9%88%d8%b2%d8%b4-%d9%86%d8%b5%d8%a8-laravel-ui-%d8%a7%d8%ad%d8%b1%d8%a7%d8%b2-%d9%87%d9%88%db%8c%d8%aa-%d9%82%d8%af%db%8c%d9%85%db%8c-%d9%84%d8%a7%d8%b1%d8%a7%d9%88%d9%84/
+//Auth::routes();
 
-Auth::routes();
+Auth::routes(['register' => false]);
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
