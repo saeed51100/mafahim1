5596b262c4855875a38d2095723899dd5b0fa405
saeed <saeed@saeed.com>
2021-11-18 09:04:52 +0330

    edare

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    // HasFactory for database testing saeeddoc.
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
