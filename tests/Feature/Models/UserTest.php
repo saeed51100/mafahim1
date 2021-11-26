185b38398edce4d3672c91e6d9e91d91586bb3f1
saeed <saeedblack@gmail.com>
2021-11-26 14:37:24 -0500

    add first test but error when RefreshDatabase

---
+++ tests/Feature/Models/UserTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testInsertData()
+    {
+        User::factory()->create();
+
+        $this->assertDatabaseCount('users', '1');
+    }
+}
